bbc7cd8153298ec5a1e35c94076e902d428b142e
Hanze Chen <tony.chen424@gmail.com>
2019-10-08 20:21:00 -0500

    finish background running

---
+++ Type.h
@@ -0,0 +1,12 @@
+//
+// Created by 陈瀚泽 on 10/8/19.
+//
+
+#ifndef OSH_TYPE_H
+#define OSH_TYPE_H
+
+typedef enum {
+    background_operation, front_operation, change_directory, exit_shell
+}operation_t;
+
+#endif //OSH_TYPE_H
--- main.c
+++ main.c
@@ -1,76 +1,100 @@
 //
-// Created by 陈瀚泽 on 2019-06-07.
+// Created by 陈瀚泽 on 10/8/19.
 //
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <limits.h>
+#include "Type.h"
 #include <string.h>
-#include <sys/wait.h>
-#include <sys/types.h>
-#define MAXLINE 80
+#include <zconf.h>
+#include <stdio.h>
 
+operation_t command_handler(char * args[], int argument_size){
+    if(strcmp(args[0],"cd") == 0)
+        return change_directory;
+    if (strcmp(args[0],"exit") == 0)
+        return exit_shell;
+    if(strstr(args[argument_size - 1],"&"))
+        return background_operation;
+    return front_operation;
+}
 
-int main(void)
-{
-    char *args[MAXLINE/2 + 1]; /* command line arguments */
-    int shouldrun = 1; /* flag to determine when to exit program */
-    while (shouldrun) {
-        char *arg[128];
-        int heastory = 0;
-        bzero(arg, sizeof(char*)*128);
+void clean_args(char* args[], int argument_size){
+    for (int i = 0; i < argument_size ; ++i) {
+        if(args[i])
+            free(args[i]);
+    }
+}
+
+int command_parser(char * command, char *args[]){
+    //split the command
+    int head_index =0;
+    int number_argument = 0;
+    for(unsigned long i = 0; i < strlen(command); i++){
+        if (command[i] == '\n')
+            return number_argument;
+        //remove all possible spaces
+        while (command[i] == ' '){
+            i ++;
+            head_index ++;
+        }
+
+        //find the end index for next argument
+        while (command[i + 1] != ' ' && command[i + 1] != '\n')
+            i ++;
+
+        //allocate the space for array
+        args[number_argument] = malloc(sizeof(char) * (i - head_index + 1));
+
+        //write argument to args
+        strncpy(args[number_argument],command + head_index,i - head_index + 1);
+
+        number_argument++;
+        head_index = i + 1;
+    }
+    return number_argument;
+}
+
+int main(void){
+
+    char * args[128];
+    memset(args,0, 128 * sizeof(char *));
+
+    while (1) {
+        pid_t child = 0;
+        int run_flag = 0;
+        int argument_size = 0;
         char command[sysconf(_SC_ARG_MAX)];
 
         printf("osh>");
         fflush(stdout);
 
-        reset:
         //read commend as [command] and [argument]
-        fgets (command, sysconf(_SC_ARG_MAX), stdin);
+        fgets(command, sysconf(_SC_ARG_MAX), stdin);
 
-        args[heastory] = malloc(sizeof(&command));
-        strcpy(args[heastory],command);
-
-        if(heastory++ >= MAXLINE/2)
-            heastory = 0;
-
-        //split the command
-        int headindex =0;
-        char * tmp_char = strtok(strtok(command,"\n")," ");
-        while (tmp_char){
-            arg[headindex] = malloc(strlen(tmp_char) * sizeof(char));
-            strcpy(arg[headindex],tmp_char);
-            tmp_char = strtok (NULL," ");
-            headindex++;
+        argument_size = command_parser(command, &args);
+        switch (command_handler(args,argument_size)) {
+            case exit_shell:
+                return 1;
+            case change_directory:
+                if (chdir(args[0]) == -1)
+                    printf("folder %s  is not exsist", args[1]);
+                break;
+            case background_operation:
+                run_flag = 1;
+                free(args[argument_size -1]);
+                args[argument_size - 1] = NULL;
+            case front_operation:
+                child = fork();
+                if (child == 0) {
+                    if (execvp(args[0],args) == -1)
+                        printf("command %s not found \n", args[0]);
+                } else {
+                    if (run_flag == 0)
+                        wait(NULL);
+                    else
+                        wait(200); //wait a small amount time for let execvp run
+                }
+                break;
         }
 
-        if(strcmp(arg[0], "cd") == 0){
-            if (chdir(arg[1]) == -1)
-                printf("folder %s  is not exsist", arg[1]);
-        }else if (arg[0][0] == '!'){
-            if(arg[0][1] == '!')
-                write(stdin, args[heastory], sizeof(&args[heastory]));
-            else
-                write(STDIN_FILENO, args[arg[0][0]], sizeof(&args[arg[0][0]]));
-            write(STDIN_FILENO, "\n", 1);
-            goto reset;
-        }else if(strcmp(arg[0], "exit") == 0)
-            shouldrun = 0;
-        else{
-            pid_t child = fork();
-            if(child == 0){
-                if (execvp(arg[0], arg) == -1)
-                    printf("command %s not found \n", arg[0]);
-            }else
-                wait(NULL);
-        }
-    /**
-        After reading user input, the steps are:
-            (1) fork a child process using fork()
-            (2) the child process will invoke execvp()
-            (3) if command included &, parent will invoke wait()
-    */
+        clean_args(args,argument_size);
     }
-    return 0;
 }
